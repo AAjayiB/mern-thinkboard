fee093d892603946065af9b3a0d0b2caab2899fa
Ameen <72974478+AAjayiB@users.noreply.github.com>
2025-06-26 02:14:37 +0100

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,33 +1,41 @@
 import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import path from 'path'
 import notesRoutes from './routes/notesRoutes.js'
 import { connectDB } from './config/db.js'
-import dotenv from 'dotenv'
 import ratelimiter from './middleware/rateLimiter.js'
-import cors from 'cors'
+
 
 dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 5001
-
+const __dirname = path.resolve()
 
 
 
 // middleware
-app.use(cors({
-    origin:'http://localhost:5173',
-}))
+if(process.env.NODE_ENV !=='production'){
+    app.use(cors({
+        origin:'http://localhost:5173',
+    }))
+}
 app.use(express.json()) // parses json body
 app.use(ratelimiter)
 
-
-// custom middleware
-// app.use((req,res,next) =>{
-//     console.log(`Request method is ${req.method} and Request URL is ${req.url}`)
-//     next()
-// })
-
 app.use('/api/notes',notesRoutes)
+
+
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname,'../frontend/dist')))
+
+    app.get("/*splat", (req,res) =>{
+        res.sendFile(path.join(__dirname,'../frontend','dist','index.html'))
+    })
+}
+
 
 connectDB().then(()=>{
     app.listen(PORT, () =>{
